93b22bce68965f18e38d64a86142788c84d633c9
Delivion93 <artem.abramov.1993@gmail.com>
2024-11-12 13:58:37 +0100

    feature: js sheet01 ex07 complete part1

---
+++ JavaScript/Sheet01/Ex07/script.js
@@ -0,0 +1,12 @@
+const diasDeLaSemana = [
+  "lunes",
+  "martes",
+  "miércoles",
+  "jueves",
+  "viernes",
+  "sábado",
+  "domingo",
+];
+for (let i in diasDeLaSemana) {
+  console.log(diasDeLaSemana[i]);
+}
