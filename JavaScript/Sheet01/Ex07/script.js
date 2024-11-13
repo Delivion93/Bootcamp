693a875284641a465d402c020731131ce14dc7cf
Delivion93 <artem.abramov.1993@gmail.com>
2024-11-13 09:48:08 +0100

    feature: js sheet01 ex07 complete part2

--- JavaScript/Sheet01/Ex07/script.js
+++ JavaScript/Sheet01/Ex07/script.js
@@ -10,3 +10,16 @@ const diasDeLaSemana = [
 for (let i in diasDeLaSemana) {
   console.log(diasDeLaSemana[i]);
 }
+
+function sumarArreglo(array) {
+  let result = 0;
+  for (let i in array) {
+    result += array[i];
+  }
+  return result;
+}
+
+let array = [1, 2, 3, 4, 5];
+console.log(
+  `resultado de llamada sumarArreglo(array) es ${sumarArreglo(array)}`
+);
