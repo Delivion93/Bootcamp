b5395f35e83bf8b2f04aedca649f4313a56f4304
Delivion93 <artem.abramov.1993@gmail.com>
2024-11-13 11:45:20 +0100

    fix: structure of exercices 8-15

--- JavaScript/Sheet01/structure.sh
+++ JavaScript/Sheet01/structure.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-for i in {1..15}
+for i in {8..15}
 do
         folder=$(printf "Ex%02d" $i)
         mkdir "$folder"
         cd "$folder"
-        touch script.js
+        touch index.html
         cd ..
 done
