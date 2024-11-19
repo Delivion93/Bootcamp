2f6480f4e619474fc6c006962aa6d0bedd2c12a8
Delivion93 <artem.abramov.1993@gmail.com>
2024-11-19 20:38:38 +0100

    feature: js sheet05 ex01 add DOMcontentLoader

--- JavaScript/Sheet05/Ex01/script.js
+++ JavaScript/Sheet05/Ex01/script.js
@@ -1,82 +1,89 @@
-{
-  /* <div class="card"> +
-          <div class="card-top"> 
-            <img
-              src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
-            />
-            <p class="ID">ID/1</p>+
-          </div>
-          <div class="card-bottom">
-            <h2>Bulbasaur</h2>+
-            <ul class="type-list">+
-              <li>poison</li>
-              <li>grass</li>
-            </ul>
-            <div class="evolution"> +
-              <p>Evoluciona de:</p>
-              <p class="evolution-name">bulbasaur</p>
-            </div>
-          </div>
-        </div> */
-}
-// function createCard(img, id, name, types, evolution)
-function createCard(img, id, name, types) {
-  const grid = document.getElementById("grid");
-  const card = document.createElement("div");
-  card.className = "card";
-  const cardTop = document.createElement("div");
-  cardTop.className = "card-top";
-  const image = document.createElement("img");
-  image.src = img;
-  image.alt = name;
-  const pId = document.createElement("p");
-  pId.className = "ID";
-  pId.textContent = `ID / ${id}`;
-  const cardBottom = document.createElement("div");
-  cardBottom.className = "card-bottom";
-  const h2 = document.createElement("h2");
-  h2.textContent = name;
-  const list = document.createElement("ul");
-  list.className = "type-list";
-  const evolutionDiv = document.createElement("div");
-  evolutionDiv.className = "evolution";
-  const evolutionP = document.createElement("p");
-  const evolutionName = document.createElement("p");
-  evolutionName.className = "evolution-name";
+document.addEventListener("DOMContentLoaded", () => {
+  function createCard(img, id, name, types, evolution) {
+    const grid = document.getElementById("grid");
+    const card = document.createElement("div");
+    card.className = "card";
+    const cardTop = document.createElement("div");
+    cardTop.className = "card-top";
+    const image = document.createElement("img");
+    image.src = img;
+    image.alt = name;
+    const pId = document.createElement("p");
+    pId.className = "ID";
+    pId.textContent = `ID / ${id}`;
+    const cardBottom = document.createElement("div");
+    cardBottom.className = "card-bottom";
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const list = document.createElement("ul");
+    list.className = "type-list";
 
-  grid.appendChild(card);
-  card.appendChild(cardTop);
-  card.appendChild(cardBottom);
-  cardTop.appendChild(image);
-  cardTop.appendChild(pId);
-  cardBottom.appendChild(h2);
-  cardBottom.appendChild(list);
-  types.forEach((type) => {
-    const typeItem = document.createElement("li");
-    typeItem.textContent = type.type.name;
-    list.appendChild(typeItem);
-  });
-  cardBottom.appendChild(evolutionDiv);
-  evolutionDiv.appendChild(evolutionP);
-  evolutionDiv.appendChild(evolutionName);
-}
+    grid.appendChild(card);
+    card.appendChild(cardTop);
+    card.appendChild(cardBottom);
+    cardTop.appendChild(image);
+    cardTop.appendChild(pId);
+    cardBottom.appendChild(h2);
+    cardBottom.appendChild(list);
+    types.forEach((type) => {
+      const typeItem = document.createElement("li");
+      typeItem.textContent = type.type.name;
+      list.appendChild(typeItem);
+    });
+    if (evolution != null) {
+      const evolutionDiv = document.createElement("div");
+      evolutionDiv.className = "evolution";
+      cardBottom.appendChild(evolutionDiv);
+      const evolutionP = document.createElement("p");
+      evolutionP.textContent = "Evoluciona de:";
+      const evolutionName = document.createElement("p");
+      evolutionName.className = "evolution-name";
+      evolutionName.textContent = evolution.name;
+      evolutionDiv.appendChild(evolutionP);
+      evolutionDiv.appendChild(evolutionName);
+    }
+  }
 
-async function getPokemons() {
-  let pokemons = [];
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-  const data = await response.json();
+  async function getPokemons() {
+    let pokemons = [];
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const data = await response.json();
 
-  const pokemonPromises = data.results.map(async (element) => {
-    const pokemonResponse = await fetch(element.url);
-    const pokemonInfo = await pokemonResponse.json();
-    const name = pokemonInfo.name;
-    const image = pokemonInfo.sprites.front_default;
-    const id = pokemonInfo.id;
-    const types = pokemonInfo.types;
-    return { img: image, id: id, name: name, type: types };
-  });
-  pokemons = await Promise.all(pokemonPromises);
-  pokemons.forEach((pokemon) => {
-    createCard(pokemon.img, pokemon.id, pokemon.name, pokemon.type);
-  });
-}
+    const pokemonPromises = data.results.map(async (element) => {
+      const pokemonResponse = await fetch(element.url);
+      const pokemonInfo = await pokemonResponse.json();
+      const name = pokemonInfo.name;
+      const image = pokemonInfo.sprites.front_default;
+      const id = pokemonInfo.id;
+      const types = pokemonInfo.types;
+      const speciesResponse = await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${id}/`
+      );
+      const speciesInfo = await speciesResponse.json();
+      const evolution = speciesInfo.evolves_from_species;
+      return {
+        img: image,
+        id: id,
+        name: name,
+        type: types,
+        evolution: evolution,
+      };
+    });
+    pokemons = await Promise.all(pokemonPromises);
+    return pokemons;
+  }
+
+  async function drawGrid() {
+    const pokemons = await getPokemons();
+    pokemons.forEach((pokemon) => {
+      createCard(
+        pokemon.img,
+        pokemon.id,
+        pokemon.name,
+        pokemon.type,
+        pokemon.evolution
+      );
+    });
+  }
+  drawGrid();
+});
