6620077773f31b7eee59963c0b322d37367716b6
Delivion93 <artem.abramov.1993@gmail.com>
2024-11-25 11:14:00 +0100

    feature: react sheet02 ex01 complete

--- react/sheet02/ex01/src/App.jsx
+++ react/sheet02/ex01/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const plus = () => {
+    setCount((count) => count + 1);
+  };
+  const minus = () => {
+    setCount((count) => count - 1);
+  };
 
+  const reset = () => {
+    setCount((count) => 0);
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      Count is {count};
+      <div className="buttons">
+        <button onClick={plus} name="">
+          Plus
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={minus}>Minus</button>
+        <button onClick={reset}>Reset</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
