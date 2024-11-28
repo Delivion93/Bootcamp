cdd99af72146007240ef90da0597eee571dabe85
Delivion93 <artem.abramov.1993@gmail.com>
2024-11-28 12:02:22 +0100

    feature: java sh01 ex02 complete

---
+++ Java/sh01/ex02/VariablesTipos.java
@@ -0,0 +1,8 @@
+class VariablesTipos {
+    public static void main(String[] args) {
+        String text = "Some text";
+        int number = 15;
+        System.out.println("value de number es: "+number);
+        System.out.println("value de text es "+text);
+    }    
+}
