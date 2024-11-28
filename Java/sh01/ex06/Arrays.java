fac4217281ae8ea24b0e0ae331ea57da02107670
Delivion93 <artem.abramov.1993@gmail.com>
2024-11-28 13:45:57 +0100

    feature: java sh01 ex06 complete

---
+++ Java/sh01/ex06/Arrays.java
@@ -0,0 +1,11 @@
+// Ejercicio 6: Arrays
+// Declara un array de enteros con 5 elementos, asígnales valores y luego
+// imprime cada elemento del array. Llama a la clase Arrays .
+public class Arrays {
+    public static void main(String[] args) {
+        int [] array = {5,1,52,65,12};
+        for (int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
