7280b9b631b90c2cbab60b82f0d41be3bd6b3437
delivion93 <artem.abramov.1993@gmail.com>
2024-12-07 19:16:26 +0100

    feature: java sh04 ex01 complete

---
+++ Java/sh04/src/ex01/Book.java
@@ -0,0 +1,57 @@
+package ex01;
+
+import java.text.DecimalFormat;
+
+public class Book {
+    private static final float PRINT_PAGE_PRICE = 0.05f;
+    private  String isbn;
+    private  String title;
+    private  int numberOfPages;
+    private  String author;
+    private boolean borrowed;
+
+    public Book(){
+
+    }
+
+    public Book(String isbn, String title, int numberOfPages, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.numberOfPages = numberOfPages;
+        this.author = author;
+    }
+
+    public void borrow(){
+        if (!borrowed){
+            borrowed=true;
+        }else{
+            System.out.println("This book isn't available");
+        }
+    }
+
+    public void returnBook(){
+        if(borrowed){
+            borrowed=false;
+        }else{
+            System.out.println("this book is in the library");
+        }
+    }
+
+    public void print(){
+        double price = (double)PRINT_PAGE_PRICE*numberOfPages;
+        DecimalFormat df = new DecimalFormat("#.00");
+        String formatedPrice = df.format(price);
+        System.out.printf("total price for printing \"%s\" is %s euro",title,formatedPrice);
+    }
+
+    @Override
+    public String toString() {
+        String state;
+        if(this.borrowed){
+            state="borrowed";
+        }else{
+            state="in the library";
+        }
+            return "ex01.Book "+isbn+" with title "+title+" and author "+author+" has "+numberOfPages+" pages and is "+state;
+    }
+}
---
+++ Java/sh04/src/ex02/PC.java
@@ -0,0 +1,78 @@
+package ex02;
+
+import exeptions.IncorrectRamException;
+
+public class PC {
+    private String brand;
+    private String model;
+    private int gbOfRam;
+    private int gbOfDiskStorage;
+    private int gbOfStorageUsing;
+    private boolean isEnabled;
+
+    public PC() {
+    }
+
+    public PC(int gbOfRam) throws IncorrectRamException {
+        if(gbOfRam%2!=0){
+            throw new IncorrectRamException("incorrect amount of memory");
+        }
+        this.gbOfRam = gbOfRam;
+    }
+
+    public PC(int gbOfRam, int gbOfDiskStorage) throws IncorrectRamException {
+        this(gbOfRam);
+        this.gbOfDiskStorage = gbOfDiskStorage;
+    }
+
+    public PC(String brand, String model, int gbOfRam, int gbOfDiskStorage) throws IncorrectRamException {
+        this(gbOfRam);
+        this.brand = brand;
+        this.model = model;
+        this.gbOfDiskStorage = gbOfDiskStorage;
+    }
+
+    public void turnOn(){
+        if(!isEnabled){
+            isEnabled = true;
+            System.out.println("starting Pc");
+        }else{
+            System.out.println("Pc is already turned on");
+        }
+    }
+    public void turnOff(){
+        if(isEnabled){
+            isEnabled = false;
+            System.out.println("turning off Pc");
+        }else{
+            System.out.println("Pc is already turned off");
+        }
+    }
+
+    public void transferFiles(int filesSize){
+        if(isEnabled){
+            if((gbOfStorageUsing+filesSize)>gbOfStorageUsing){
+                System.out.println("not enough storage space");
+            }else {
+                gbOfStorageUsing+=filesSize;
+                System.out.printf("done\n%d Gb left on disk",gbOfDiskStorage-gbOfStorageUsing);
+            }
+        }else{
+            System.out.println("Pc is turned off");
+        }
+    }
+
+    public void deleteFiles(int filesSize){
+        if(isEnabled){
+            if((gbOfStorageUsing+filesSize)>gbOfStorageUsing){
+                gbOfStorageUsing=0;
+            }else {
+                gbOfStorageUsing-=filesSize;
+                System.out.printf("done\n%d Gb left on disk",gbOfDiskStorage-gbOfStorageUsing);
+            }
+        }else{
+            System.out.println("Pc is turned off");
+        }
+    }
+
+}
