40b75615de86661cba6d90e402e33f9a4e1a4685
Delivion93 <artem.abramov.1993@gmail.com>
2024-12-18 09:43:56 +0100

    fix: SpringBoot sh01 ex01 fix method delete

--- SpringBoot/tareas/src/main/java/com/ejemplo/tareas/controllers/TareaController.java
+++ SpringBoot/tareas/src/main/java/com/ejemplo/tareas/controllers/TareaController.java
@@ -8,8 +8,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/tareas")
 public class TareaController {
-    private List<String> tareas = new ArrayList<>();
 
+    private List<String> tareas = new ArrayList<>();
+    
     @GetMapping
     public List<String> getTareas(){
         return tareas;
@@ -21,20 +22,19 @@ public class TareaController {
             return tareas.get(index);
         }
         return "index no valido";
-
     }
 
     @PostMapping
     public String addTarea(@RequestBody String tarea){
         tareas.add(tarea);
-        return "Ha agregada esta tarea "+tarea;
+        return String.format("Ha agregada esta tarea \"%s\"",tarea);
     }
 
     @DeleteMapping("/{index}")
     public String deleteTarea(@PathVariable int index){
         if(index>=0&&index<tareas.size()){
             String tareaEliminada = tareas.remove(index);
-            return String."Tarea eliminada "+tareaEliminada;
+            return String.format("Tarea eliminada \"%s\"",tareaEliminada);
         }
         return "index no valido";
     }
