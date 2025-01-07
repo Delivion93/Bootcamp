e7954aa638bd3e8d8b81cbbdf53910ca3802502a
delivion93 <artem.abramov.1993@gmail.com>
2025-01-07 22:23:29 +0100

    feature: registration

--- FinalProject/backEnd/src/main/java/com/example/backEnd/Security/SecurityConfig.java
+++ FinalProject/backEnd/src/main/java/com/example/backEnd/Security/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.example.backEnd.Security;
 
+import com.example.backEnd.Model.MyUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -11,9 +18,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    private final MyUserService myUserService;
+
+    @Autowired
+    public SecurityConfig(MyUserService myUserService) {
+        this.myUserService = myUserService;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return myUserService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(myUserService);
+        return provider;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(httpForm -> {
                     httpForm
                             .loginPage("/login").permitAll();
