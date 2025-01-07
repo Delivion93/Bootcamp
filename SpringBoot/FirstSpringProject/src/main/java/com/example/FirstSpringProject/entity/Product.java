fb09a4c416ae449689618cb55953e9a30bd7b4b2
delivion93 <artem.abramov.1993@gmail.com>
2025-01-07 11:14:45 +0100

    feature: final project back-end initial commit

---
+++ SpringBoot/FirstSpringProject/src/main/java/com/example/FirstSpringProject/entity/Product.java
@@ -0,0 +1,4 @@
+package com.example.FirstSpringProject.entity;
+
+public class Product {
+}
---
+++ SpringBoot/FirstSpringProject/src/main/java/com/example/FirstSpringProject/repository/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.example.FirstSpringProject.repository;
+
+public interface ProductRepository {
+}
